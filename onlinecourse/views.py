d457a54ec8d5a6cc97699c67758b407f685f0c67
Salva Roca <salva.roca@icloud.com>
2023-02-20 10:28:31 -0500

    Lab 3 completed

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -149,7 +149,7 @@ def show_exam_result(request, course_id, submission_id):
 
     grade = 0
 
-    for choice in choices:
+    for choice in choices:      
         if choice.is_correct:
             grade += choice.question.grade
 
